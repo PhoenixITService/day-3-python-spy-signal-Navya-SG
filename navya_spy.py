3233cee71559b9eeac9be8e24993246643ebec8f
Navya-SG <Navya.Gokila@Coforge.com>
2025-07-16 18:20:50 +0530

    Update navya_spy.py

--- navya_spy.py
+++ navya_spy.py
@@ -1,7 +1,7 @@
-input=list(map(int,input("enter numbers:").split()))
+input=input("enter numbers:").split()
 select=input[1::3]
-reverse=select[::-1]
-result=[]
-for num in reverse:
-    result+=chr(num)
-print("".join(result))
+reversed=select[::-1]
+result=""
+for num in reversed:
+    result+=chr(int(num))
+print(result)
